3cef58086e71dd5e098144e006b069c4d61f3cff
xyy601chenyan <xyy545688506@gmail.com>
2017-10-25 12:06:06 +0800

    add markdown to project

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+
+  def markdown(text)
+    options = {
+      filter_html:     true,
+      hard_wrap:       true,
+      link_attributes: { rel: 'nofollow', target: "_blank" },
+      space_after_headers: true,
+
+    }
+
+    extensions = {
+      autolink:           true,
+      superscript:        true,
+      fenced_code_blocks: true,
+      disable_indented_code_blocks: true,
+      lax_spacing: true,
+      tables: true,
+    }
+    if text.blank?
+      nil
+    else
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+    markdown.render(text).html_safe
+  end
+end
+
+
 end
